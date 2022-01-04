a30b15413e4e8d1db999d0030a103c8fe08e357c
SATHISHkesavan98 <97013943+SATHISHkesavan98@users.noreply.github.com>
2022-01-04 15:36:49 +0530

    Update MianClass.java
    added by sathish

--- MianClass.java
+++ MianClass.java
@@ -4,6 +4,7 @@ public class MianClass {
 
 	public static void main(String[] args) {
 		System.out.println("WELCOME TO HOSPITAL MANAGEMENT SYSTEM");
+		System.out.println("book here for appoinment"):
 
 	}
 
